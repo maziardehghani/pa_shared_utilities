23d3cb0cd6d5fca4dc82a5ab7b400e5a86f1fa09
maziardehghani <maziardehghani1380@gmail.com>
2026-06-08 16:14:02 +0330

    create user statuses enum

---
+++ dist/enums/user-status.enum.d.ts
@@ -0,0 +1,5 @@
+export declare enum UserStatus {
+    ACTIVE = "active",
+    BANNED = "banned",
+    PENDING = "pending"
+}
---
+++ dist/src/enums/user-status.enum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserStatus = void 0;
+var UserStatus;
+(function (UserStatus) {
+    UserStatus["ACTIVE"] = "active";
+    UserStatus["BANNED"] = "banned";
+    UserStatus["PENDING"] = "pending";
+})(UserStatus || (exports.UserStatus = UserStatus = {}));
